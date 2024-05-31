98744b5ebcc611e737a35118af21ef6e6c8ad1c9
Dustin Lennon <dustin.lennon@stelth2000inc.com>
2024-05-30 21:49:09 -0400

    Load more products

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoadMoreData from './components/load-more-data';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Load More Products Components */}
+      <LoadMoreData />
+    </div>
+  );
+}
+
+export default App;
